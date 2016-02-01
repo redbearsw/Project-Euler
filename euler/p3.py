6d9aec86f35b0dad699a157ccf6b1df9b07a99a7
Sophie Welber <Sophie@wireless-s1-so-150-52-71.uchicago.edu>
2016-02-01 15:36:04 -0600

    Working on listing prime factors

--- euler/p3.py
+++ euler/p3.py
@@ -1,32 +1,27 @@
-fact = []
+fact = [1]
+a = 2
 
-def factors(x):
-    for i in range (1, 100):
-        if x%i == 0:
-            fact.append(i)
-            print " %d is a factor" %i
-        
-       
-factors(13195)
+def primefactors(x):
+    while a < 100:
+        if x%a == 0 and x/a != 1:
+            primefactors(x/a)
+        else:
+            fact.append(x)
+            a += 1
+            primefactors(x)
+
+
 
 print fact
 
+primefactors(10)
+
+
 def primefactors():
     print fact.pop()
-    while len(fact) != 0:    
-        for i in range (1,1000): 
-            if fact.pop()%i ==0:
+    while len(fact) != 0:
+        for i in range (1,1000):
+            if fact.pop()%i == 0:
                 print fact.pop()/i
             else:
                 print fact.pop()
-            
-primefactors()
-       
-
-    
-    
-    
-
-
-    
-    
