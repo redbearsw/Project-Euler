7f0552303b1c165fb42fef0d55d6863f0bda112f
Sophie Welber <Sophie@wireless-s1-so-150-50-27.uchicago.edu>
2016-02-02 00:02:53 -0600

    FUCKKKKKKKK

--- euler/p3.py
+++ euler/p3.py
@@ -1,28 +1,46 @@
-fact = [1]
-a = 2
 
+fact=[1]
+#makes a list of factors of x
 def factors(x):
     for i in range (2, x+1):
         if x%i == 0 and x/i != 1:
             fact.append(i)
         else:
-            print "%d is not a factor of %d" %(i, x)
-    print fact
+            return fact
 
 factors(36)
 
-primes = []
+print fact
 
-#want to make this function take in a list of factors
-def primefactors(x):
-    for p in fact:
-        for i in range (2,x+1):
-            if p%i == 0 and p/i != 1:
-                primefactors(p/i)
-                print "checking %d now" %p/i
-            else:
-                primes.append(p)
-                print "appending %d" %p
-    print primes
 
-primefactors(36)
+
+
+#only keeps the prime factors of x
+def prime(x):
+    for i in range (2,x+1):
+        if x%i == 0 and x/i != 1:
+            prime(x/i)
+        else:
+            return x
+#tests
+prime(1)
+prime(2)
+prime(3)
+prime(5)
+prime(6)
+prime(7)
+prime(8)
+prime(9)
+prime(10)
+
+
+
+primefactors = []
+def primefactors():
+    for i in fact:
+        if prime(i):
+            return i
+        else:
+            print "%d is not a primefactor" %i
+
+primefactors()
