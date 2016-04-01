8253ad293923b4d4bed5f3c6b116f788dd5c3df3
redbearsw <sjwelber@gmail.com>
2016-03-31 19:20:51 -0500

    Finished p3, with my own prime? function

--- euler/p3.rkt
+++ euler/p3.rkt
@@ -2,20 +2,27 @@
 
 (require math/number-theory)
 
-(: largest-prime : Real -> Real)
-(define (largest-prime num) (local {
-	(: larg-prime : Real Real -> Real)
-	(define (larg-prime n div) (cond
-		[(= (/ n div) 0) div]
-		[(integer? (/ n div)) (larg-prime (/ n div) div)]
-		[else (larg-prime n (+ 1 div))]))}
-	(larg-prime num 2)))
+(: soph-prime? : Integer -> Boolean)
+(define (soph-prime? int) (local {
+	(: div-check : Integer Integer -> Boolean)
+	(define (div-check i div) (cond 
+		[(= i div) #t]
+		[(= (modulo i div) 0) #f]
+		[else (div-check i (+ div 1))]))}
+	(div-check int 2)))
 
-(largest-prime 12)
-(largest-prime 20)
-	
+(: big-prime-fact : Integer -> Integer)
+(define (big-prime-fact num) (local {
+	(: prime-facts : Integer Integer (Listof Integer)  -> (Listof Integer))
+	(define (prime-facts n div acc) (cond
+		[(= n div) (cons div acc)]
+		[(and (= (modulo n div) 0) (soph-prime? div)) (prime-facts (ceiling (/ n div)) div (cons div acc))]
+		[else (prime-facts n (+ div 1) acc)]))}
+	(first (prime-facts num 2 '())))) 
 
-;start with num
-;if prime, check if modulo =0
-;if both true, cons
-;else check next number
+(big-prime-fact 2)
+(big-prime-fact 12)
+(big-prime-fact 20)
+(big-prime-fact 300)
+(big-prime-fact 600851475143)
+
