b00a9a99ea8b760936a7e927aef9f2de5b69c3f1
Sophie Welber <sjwelber@gmail.com>
2016-06-15 15:27:53 -0400

    p6 and 7 done

--- euler/p6.c
+++ euler/p6.c
@@ -8,9 +8,17 @@ int sum_sq_diff(int num) {
 		return sum_sq_diff(num - 1) + ((num * num) * (num - 1));
 }
 
+int sum_sq_diff_it(int num) {
+	int i; int total = 0;
+	for (i = 1; i <= num; i++)
+		total += ((num * num) * (num - 1));
+	return total;
+}		
+
 int main() {
 	int d5 = sum_sq_diff(5);
 	int d10 = sum_sq_diff(10);
 	int d100 = sum_sq_diff(100);
-	printf("5: %d, 10: %d, 100: %d\n",d5,d10,d100);
+	int d100i = sum_sq_diff_it(100);
+	printf("5: %d, 10: %d, 100: %d or %d\n",d5,d10,d100,d100i);
 }
