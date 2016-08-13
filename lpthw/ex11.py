688f97c85d9c2b5c2a3225de50d7f2e2dcf21eea
Sophie Welber <sjwelber@gmail.com>
2016-08-13 13:49:24 -0400

    restarting lpthw

---
+++ lpthw/ex11.py
@@ -0,0 +1,8 @@
+print "backspace: sophie\bnatalie"
+print "carriage return: \rnat"
+print "formfeed: sophie\fnatalie"
+print "octal value 000: \ooo"
+
+while True:
+	for i in ["/", "-", "|", "\\", "|"]:
+		print "%s\r" % i
---
+++ lpthw/ex12.py
@@ -0,0 +1,12 @@
+print "How old are you?"
+age = raw_input()
+print "How tall are you?"
+height = raw_input()
+print "How much do you weigh?"
+weight = raw_input()
+
+print "So, you're %s years old, %r tall and %r heavy." % (age, height, weight)
+
+number1 = int(raw_input("give me a number "))
+number2 = int(raw_input("give me a number "))
+print number1 + number2
