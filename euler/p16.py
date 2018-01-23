94be5e0afa8919d8457e9dac1b51aad8f7d852cd
Sophie Welber <sjwelber@gmail.com>
2018-01-22 19:37:02 -0600

    Added comments to p11 and p16

--- euler/p16.py
+++ euler/p16.py
@@ -1,6 +1,8 @@
 def find_sum(base, exp):
     num = long(base ** exp)
+    #start with ones digit
     sum = num % 10
+    #add each subsequent digit by dividing by ten and isolating
     while (num > 0):
         sum += num / 10 % 10
         num /= 10
