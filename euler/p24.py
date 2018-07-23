2b58fa48fb2b37a8f5d0f021410879eff5163a8e
Sophie Welber <sjwelber@gmail.com>
2018-07-22 23:36:21 -0500

    theory on how to solve p24.py without computer

---
+++ euler/p24.py
@@ -0,0 +1,5 @@
+# there are 9! options starting with 0, 1, etc
+# add 9! until above 1000 --> 3
+# so, must be in the range where the first digit is 1
+# now add 9! + 9! + 8! ... until above 1000
+# etc
