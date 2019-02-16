c50b2378ebd84be75d2fbef7c0f7ce1e1d188a30
Sophie Welber <sjwelber@gmail.com>
2019-02-15 20:07:40 -0600

    started day1

--- advent/2017/day1/day1.c
+++ advent/2017/day1/day1.c
@@ -1,3 +1,20 @@
-#include "stdio.h"
-#include "stdlib.h"
-FILE *input = fopen("day1.txt");
+#include <stdio.h>
+#include <stdlib.h>
+
+void get_input(char* name) {
+    FILE *input = fopen(name,"r");
+    if (input == NULL) {
+        fprintf(stderr, "Can't open file %s\n", name);
+        return;
+    }
+    char c;
+    while (fscanf(input, "%c", &c) != EOF) {
+        // add to array
+    }
+    fclose(input);
+    return;
+}
+
+int main() {
+    get_input("day1.txt");
+}
