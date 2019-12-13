b7b9a09f7f47e3ca74ddb59e294c70793f2d403e
Sophie Welber <sjwelber@gmail.com>
2019-12-13 17:15:23 -0600

    created and worked on day3, building grid works for left/right but not up/down

---
+++ advent/2019/day3/Day3.java
@@ -0,0 +1,126 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
+class Day3 {
+
+  public static ArrayList<ArrayList<String>> readInput(String name) {
+    String line;
+    ArrayList<ArrayList<String>> inputs = new ArrayList<ArrayList<String>>();
+    ArrayList<String> path = new ArrayList<String>();
+    
+    // Read in file to integer array
+    try {
+      File file = new File(name);
+      Scanner sc = new Scanner(file);
+      
+      while (sc.hasNextLine()) {
+
+        line = sc.nextLine();
+        StringTokenizer tok = new StringTokenizer(line, ",");
+
+        while (tok.hasMoreTokens())
+          path.add(tok.nextToken());
+        
+        inputs.add(path);
+      }
+    }
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    return inputs;
+  }
+
+  public static ArrayList<ArrayList<String>> buildGrid(ArrayList<String> inputs) {
+    // grid starting point
+    ArrayList<ArrayList<String>> grid = new ArrayList<ArrayList<String>>();
+    grid.add(new ArrayList<String>());
+    grid.get(0).add("o");
+
+    // add wires
+    int i;
+    int j;
+    int currX = 0;
+    int currY = 0;
+    int len = inputs.size();
+    for (i = 0; i < len; i++) {
+      String instr = inputs.get(i);
+      char firstChar = instr.charAt(0);
+      int num = Integer.parseInt(instr.substring(1));
+
+      if (firstChar == 'U') {
+        for (j = currY + 1; j <= currY + num; j++)
+          try {
+            grid.get(j);
+          } catch (IndexOutOfBoundsException e) {
+            grid.set(j, new ArrayList<String>());
+          }
+          grid.get(j).set(currX, "-");
+
+        currY = currY + num;
+      }
+      else if (firstChar == 'R') {
+        for (j = currX + 1; j <= currX + num; j++) {
+          try {
+            grid.get(currY).get(j);
+          } catch (IndexOutOfBoundsException e) {
+            grid.get(currY).add("-");
+          }
+          grid.get(currY).set(j, "-");
+        }
+        currX = currX + num;
+      }
+      else if (firstChar == 'L') {
+        for (j = currX - 1; j >= currX - num; j--) {
+          try {
+            grid.get(currY).get(j);
+          } catch (IndexOutOfBoundsException e) {
+            grid.get(currY).add("-");
+          }
+          grid.get(currY).set(j, "-");
+        }
+        currX = currX - num;
+      }
+      else if (firstChar == 'D') {
+        for (j = currY - 1; j >= currY - num; j--)
+          try {
+            grid.get(j);
+          } catch (IndexOutOfBoundsException e) {
+            grid.set(j, new ArrayList<String>());
+          }
+          grid.get(j).set(currX, "-");
+
+        currY = currY - num;
+      }
+      else {
+        throw new IllegalArgumentException("Instruction given improperly");
+      }
+    }
+    return grid;
+  }
+
+  public static int part1(String name) {
+    // read inputs
+    ArrayList<ArrayList<String>> inputs = readInput(name);
+
+    // create grids from inputs
+    ArrayList<ArrayList<String>> firstGrid = buildGrid(inputs.get(0));
+    System.out.println(firstGrid);
+
+    // search grids and list intersections
+
+    // find manhattan distance from port to each intersection
+
+    // return smallest distance
+
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Part 1: " + part1("test.txt"));
+  }
+}
+
---
+++ advent/2019/day3/Tuple.java
@@ -0,0 +1,20 @@
+public class Tuple<X, Y> {
+  private X x;
+  private Y y;
+  public Tuple(X x, Y y) {
+    this.x = x;
+    this.y = y;
+  }
+  public X getX() {
+    return this.x;
+  }
+  public Y getY() {
+    return this.y;
+  }
+  public void setX(X x) {
+    this.x = x;
+  }
+  public void setY(Y y) {
+    this.y = y;
+  }
+}
